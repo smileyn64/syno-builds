b6c0f48ddb7f20c8e4a9a3ffbcd487e1eb08bc0c
Nicolas L. P <loustaunau.nicolas@gmail.com>
2021-09-22 14:36:21 +0200

    NASM & DSM 7.0 toolchain url update
    NASM & DSM 7.0 toolchain url update

--- sane-backends and AirSane.sh
+++ sane-backends and AirSane.sh
@@ -29,9 +29,9 @@ sudo alternatives --install /usr/local/bin/cmake cmake /usr/bin/cmake 10 --slave
 sudo alternatives --install /usr/local/bin/cmake cmake /usr/bin/cmake3 20 --slave /usr/local/bin/ctest ctest /usr/bin/ctest3 --slave /usr/local/bin/cpack cpack /usr/bin/cpack3 --slave /usr/local/bin/ccmake ccmake /usr/bin/ccmake3 --family cmake
 #-----to switch version use 'sudo alternatives --config cmake'
 
-wget http://www.nasm.us/pub/nasm/releasebuilds/2.13.02/nasm-2.13.02.tar.bz2
-tar xjvf nasm-2.13.02.tar.bz2
-cd nasm-2.13.02
+wget https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.bz2
+tar xjvf nasm-2.15.05.tar.bz2
+cd nasm-2.15.05
 ./autogen.sh
 ./configure
 make
@@ -40,7 +40,7 @@ cd ..
 
 #-----set up Synology toolchain
 cd ~/Downloads
-export DL_PATH="https://sourceforge.net/projects/dsgpl/files/DSM%206.0.2%20Tool%20Chains"
+export DL_PATH="https://sourceforge.net/projects/dsgpl/files/Tool%20Chain/DSM%207.0.0%20Tool%20Chains"
 
 
 #----------------------------------------------------
